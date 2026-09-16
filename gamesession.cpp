f6f1a3d9109d428e5c2a89cacd2045683ea528f8
mdmassaro <143455023+mdmassaro@users.noreply.github.com>
2026-09-16 19:18:28 +0000

    static variables and classes

---
+++ gamesession.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class GameSession{
+
+    private:
+        string IPAddress;
+        int userID;
+        static int activeGameSessions;
+
+    public:
+
+        static int getActiveSessions(){
+            return activeGameSessions;
+        }
+
+        // static int activeGameSessions;
+
+        GameSession(string ipa, int uid):IPAddress(ipa), userID(uid){
+            cout << endl << "Game Session with uderID " << userID << " and IPAddress " << IPAddress << " connected " << endl;
+            activeGameSessions += 1;
+        }
+
+        ~GameSession() {
+            cout << endl << "Game ended" << endl;
+            activeGameSessions -= 1;
+        }
+
+};
+
+int GameSession::activeGameSessions = 0;
+
+int main(){
+
+    cout << endl << "Server started..." << endl;
+    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
+    GameSession newGameSession("123.456.789.0.123", 12345);
+    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
+    GameSession anotherGameSession("123.456.789.0.456", 435);
+    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
+    cout << endl << "Server shutdown..." << endl;
+
+
+    return 0;
+}
