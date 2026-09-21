e0c0ad76b6aa15b055cba388ca60ccd9c471dc10
mdmassaro <143455023+mdmassaro@users.noreply.github.com>
2026-09-21 10:29:27 -0700

    Update gamesession.cpp

--- gamesession.cpp
+++ gamesession.cpp
@@ -18,17 +18,3 @@ GameSession::~GameSession() {
     cout << endl << "Game ended" << endl;
     activeGameSessions -= 1;
 }
-
-int main(){
-
-    cout << endl << "Server started..." << endl;
-    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
-    GameSession newGameSession("123.456.789.0.123", 12345);
-    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
-    GameSession anotherGameSession("123.456.789.0.456", 435);
-    cout << endl << "Number of game sessions: " << GameSession::getActiveSessions() << endl;
-    cout << endl << "Server shutdown..." << endl;
-
-
-    return 0;
-}
