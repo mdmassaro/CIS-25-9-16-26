46c528bb3688dc4233e99cdb429db910f8d5739e
mdmassaro <143455023+mdmassaro@users.noreply.github.com>
2026-09-21 10:25:03 -0700

    Update gamesession.h

--- gamesession.h
+++ gamesession.h
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class GameSession{
+
+    private:
+        string IPAddress;
+        int userID;
+        static int activeGameSessions;
+
+    public:
+
+        static int getActiveSessions(){
+            return activeGameSessions;
+        }
+
+        GameSession(string ipa, int uid):IPAddress(ipa), userID(uid);
+
+        ~GameSession();
+
+};
 
